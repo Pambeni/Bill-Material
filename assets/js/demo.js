12541b203e29299bbb66d0a469e062c91d32d7bb
TimothyTakudzwa <timothytakudzwa@gmail.com>
2018-07-23 11:13:29 -0700

    Push

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -933,7 +933,7 @@ demo = {
     initGoogleMaps: function(){
         var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
         var mapOptions = {
-          zoom: 13,
+          zoom: 20,
           center: myLatlng,
           scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
           styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
@@ -994,10 +994,10 @@ demo = {
 
 
         // Satellite Map
-        var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
+        var myLatlng = new google.maps.LatLng(-17.824858, 31.053028	);
         var mapOptions = {
-            zoom: 3,
-            scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+            zoom: 15,
+            scrollwheel: true, //we disable de scroll over the map, it is a really annoing when you scroll through page
             center: myLatlng,
              mapTypeId: google.maps.MapTypeId.SATELLITE
         }
@@ -1006,7 +1006,7 @@ demo = {
 
         var marker = new google.maps.Marker({
             position: myLatlng,
-            title:"Satellite Map!"
+            title:"Billboards"
         });
 
         marker.setMap(map);
